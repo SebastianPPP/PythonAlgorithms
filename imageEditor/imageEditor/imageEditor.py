27056520ec1fa14f26f8998a7383e9b3b95f183e
SebastianPPP <sebastianparzych66@gmail.com>
2023-03-14 08:38:39 +0100

    new app

--- imageEditor/imageEditor/imageEditor.py
+++ imageEditor/imageEditor/imageEditor.py
@@ -1,1 +1,21 @@
+from distutils.command.clean import clean
+from symbol import factor
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = './imgs'
+pathOut = '/editedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+    edit = img.filter(ImageFilter.SHARPEN).convert('L')
+
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+
+    clean_name = os.path.splitext(filename)[0]
+    
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
 
